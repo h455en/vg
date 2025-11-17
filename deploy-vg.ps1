e50211e4cab332909fec852cbdab70b0364e2eb0
unknown <hdoghmen@outlook.com>
2025-11-17 18:38:44 +0100

    AUTO - commit SkyScrap -

--- deploy-vg.ps1
+++ deploy-vg.ps1
@@ -1,23 +1,42 @@
 
 # VG deploy
 
-$msg = "AUTO - sky broc 15"
+
+
+$DateStr = $Date.ToString("yyyy.MM.dd_HH:ss")
+
+
+$msg = "AUTO - commit SkyScrap - " + $DateStr
+
+$scrapYml = "D:\HASSEN\WORK\PROJ\VG\GH\vg\.github\workflows\skyscrap_run.yml"
+$brocYml = "D:\HASSEN\WORK\PROJ\VG\GH\vg\.github\workflows\skyscrap_run.yml"
+$cleanYml = "D:\HASSEN\WORK\PROJ\VG\GH\vg\.github\workflows\skyscrap_run.yml"
+
+
+$scrapWf = gc $scrapYml | Select-String "name:" | Select-Object -First 1
+$brocWf = gc $brocYml | Select-String "name:" | Select-Object -First 1
+$cleanWf = gc $cleanYml | Select-String "name:" | Select-Object -First 1
+
+
+#$scrapWf = "SkyScraper v2025.11.16.3" 
+#$cleanWf = "Clean Old Workflow Runs v0.2"
+#$brocWf = "Skybroc v0.15"
+
+
 git  commit -am $msg  ; Start-Sleep -Seconds 3; git push
 
 
 Start-Sleep -Seconds 10
 
-$scrapWf = "SkyScraper v2025.11.16.3" 
-$cleanWf = "Clean Old Workflow Runs v0.2"
-$brocWf = "Skybroc v0.15"
+
 
 gh workflow run $scrapWf  --ref main
 gh workflow run $brocWf  --ref main
 
 $cutoffDate = "2025-01-15T10:00:00" #example: "2025-11-16T21:30:00"  16/11/2025 at 21:30 (Paris time).
-$cutoffDate = "2025-11-17T12:30:00"
+$cutoffDate = "2025-11-17T16:30:00"
 $status = 'failure|cancelled' #status → example: completed, success, failure, cancelled, etc.
-$status = "failure"
+$status = "completed"
 gh workflow run $cleanWf -f cutoff_datetime="$cutoffDate" -f status="$status" --ref main
 
 
