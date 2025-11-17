b332cf0307781fe4d15d0c6ca524b9841b26acb5
unknown <hdoghmen@outlook.com>
2025-11-17 12:31:24 +0100

    AUTO - sky broc 13

--- deploy-vg.ps1
+++ deploy-vg.ps1
@@ -1,20 +1,25 @@
 
 # VG deploy
 
-$msg = "AUTO - sky broc 11"
+$msg = "AUTO - sky broc 13"
 git  commit -am $msg  ; Start-Sleep -Seconds 3; git push
 
 
 Start-Sleep -Seconds 10
 
-$scrapWf = "SkyScraper v2025.11.15.1" 
+$scrapWf = "SkyScraper v2025.11.16.2" 
+$cleanWf = "Clean Old Workflow Runs v0.2"
+$brocWf = "Skybroc v0.14"
+
 gh workflow run $scrapWf  --ref main
-
-
-$brocWf = "Skybroc v0.13"
 gh workflow run $brocWf  --ref main
 
+$cutoffDate = "2025-01-15T10:00:00" #example: "2025-11-16T21:30:00"  16/11/2025 at 21:30 (Paris time).
+$cutoffDate = "2025-11-17T10:30:00"
+$status = 'failure|cancelled' #status → example: completed, success, failure, cancelled, etc.
+$status = "completed"
+#example: 2025-01-15T10:00:00 example: 2025-01-15T10:00:00 example: 2025-01-15T10:00:00 example: 2025-01-15T10:00:00
 
-$cleanWf = "Clean Old Workflow Runs v0.1"
-gh workflow run $cleanWf  --ref main
+gh workflow run $cleanWf -f cutoff_datetime="$cutoffDate" -f status="$status" --ref main
+
 
